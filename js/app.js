4c5c0240caf0f3a55dd5538d5aff4b40fb1ed994
Alison Williams <alison.lauriewill@gmail.com>
2019-05-12 20:52:48 +0100

    radio functionality added with logic-based CSS styling

--- js/app.js
+++ js/app.js
@@ -21,29 +21,8 @@ const handleFormSubmit = function (event) {
 const createPantheonListItem = function (form) {
   const pantheonListItem = document.createElement('li');
   pantheonListItem.classList.add('pantheon-list-item');
-    switch (form.domain.value) {
-      case 'Sky':
-      pantheonListItem.classList.add('sky');
-      break;
-      case 'War':
-      pantheonListItem.classList.add('war');
-      break;
-      case 'Hearth':
-      pantheonListItem.classList.add('hearth');
-      break;
-      case 'Ocean':
-      pantheonListItem.classList.add('ocean');
-      break;
-      case 'Underworld':
-      pantheonListItem.classList.add('underworld');
-      break;
-      case 'Sun':
-      pantheonListItem.classList.add('sun');
-      break;
-      case 'Earth':
-      pantheonListItem.classList.add('earth');
-      break;
-    }
+  domainColour(pantheonListItem, form.domain.value);
+  generationStyle(pantheonListItem, form.generation.value);
 
   const name = document.createElement('h2');
   name.textContent = form.name.value;
@@ -63,4 +42,44 @@ const createPantheonListItem = function (form) {
 const handleDeleteClick = function (event) {
   const pantheonList = document.querySelector('#pantheon-list');
   pantheonList.innerHTML = '';
+}
+
+const domainColour = function (element, value) {
+  switch (value) {
+    case 'Sky':
+    element.classList.add('sky');
+    break;
+    case 'War':
+    element.classList.add('war');
+    break;
+    case 'Hearth':
+    element.classList.add('hearth');
+    break;
+    case 'Ocean':
+    element.classList.add('ocean');
+    break;
+    case 'Underworld':
+    element.classList.add('underworld');
+    break;
+    case 'Sun':
+    element.classList.add('sun');
+    break;
+    case 'Earth':
+    element.classList.add('earth');
+    break;
+  }
+}
+
+const generationStyle = function (element, value) {
+  switch (value) {
+    case 'Primordial':
+    element.id = 'primordial';
+    break;
+    case 'Titan':
+    element.id = 'titan';
+    break;
+    case 'Olympian':
+    element.id = 'olympian';
+    break;
+  }
 }
