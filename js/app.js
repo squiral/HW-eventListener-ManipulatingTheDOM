97fe85872b946280f2d0adc39b87f8d1afc22223
Alison Williams <alison.lauriewill@gmail.com>
2019-05-12 18:43:43 +0100

    Delete All functionality added, CSS styling and Domain/Colour logic added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const newItemForm = document.querySelector('#new-item-form');
   newItemForm.addEventListener('submit', handleFormSubmit);
 
+  const deleteButton = document.querySelector('#delete-all');
+  deleteButton.addEventListener('click', handleDeleteClick);
+
 })
 
 const handleFormSubmit = function (event) {
@@ -10,18 +13,54 @@ const handleFormSubmit = function (event) {
   const pantheonListItem = createPantheonListItem(event.target);
   const pantheonList = document.querySelector('#pantheon-list');
   pantheonList.appendChild(pantheonListItem);
+
+  event.target.reset();
 }
 
 
 const createPantheonListItem = function (form) {
   const pantheonListItem = document.createElement('li');
   pantheonListItem.classList.add('pantheon-list-item');
+    switch (form.domain.value) {
+      case 'Sky':
+      pantheonListItem.classList.add('sky');
+      break;
+      case 'War':
+      pantheonListItem.classList.add('war');
+      break;
+      case 'Hearth':
+      pantheonListItem.classList.add('hearth');
+      break;
+      case 'Ocean':
+      pantheonListItem.classList.add('ocean');
+      break;
+      case 'Underworld':
+      pantheonListItem.classList.add('underworld');
+      break;
+      case 'Sun':
+      pantheonListItem.classList.add('sun');
+      break;
+      case 'Earth':
+      pantheonListItem.classList.add('earth');
+      break;
+    }
 
   const name = document.createElement('h2');
   name.textContent = form.name.value;
   pantheonListItem.appendChild(name);
 
+  const roman_name = document.createElement('h3');
+  roman_name.textContent = `Roman Name: ${form.roman_name.value}`;
+  pantheonListItem.appendChild(roman_name);
+
+  const domain = document.createElement('p');
+  domain.textContent = `Domain: ${form.domain.value}`;
+  pantheonListItem.appendChild(domain);
+
   return pantheonListItem;
+}
 
-
+const handleDeleteClick = function (event) {
+  const pantheonList = document.querySelector('#pantheon-list');
+  pantheonList.innerHTML = '';
 }
